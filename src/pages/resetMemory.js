bae2df6e081f098f516b218ccf678ba7d8e60ed6
hshamsadd <hshamsadd@gmail.com>
2025-06-23 02:17:33 +0200

    Delete src directory

--- src/pages/resetMemory.js
+++
@@ -1,5 +0,0 @@
-import { quizData, quizState } from '../data.js';
-export const resetInMemoryState = () => {
-  quizData.currentQuestionIndex = 0;
-  quizState.currentQuestionIndex = 0;
-};
--- src/pages/savestate.js
+++
@@ -1,4 +0,0 @@
-import { quizState } from '../data.js';
-export function saveQuizState(state) {
-  localStorage.setItem('quizState', JSON.stringify(state));
-}
